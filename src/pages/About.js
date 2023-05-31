84bd9331cbb57f0b2210814667a861f52c256cb0
smajaas <smajaas@gmail.com>
2023-05-31 18:38:39 +0530

    about and error

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure,
+        voluptate. Obcaecati officia, neque corporis vel ad eius. Saepe, iure
+        praesentium. Ipsam excepturi autem vero culpa rem totam deleniti. Porro
+        autem excepturi necessitatibus quos. Quibusdam unde saepe corrupti
+        suscipit similique laborum?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
